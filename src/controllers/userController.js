7ffb0751ce16b924ddbc921013dd4f48209a16f5
Hieu Pham <hieu02617@gmail.com>
2025-06-03 00:26:18 +0700

    download va dung cors

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,7 +19,7 @@ let handleLogin = async (req, res) => {
   return res.status(200).json({
     errCode: userData.errCode,
     message: userData.errMessage,
-    userData,
+    user: userData.user ? userData.user : {},
   });
 };
 
